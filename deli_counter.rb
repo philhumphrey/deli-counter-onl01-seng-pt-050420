d2cbd8172d0135bd1aed96b9f30e5967443790d4
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-15 14:59:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,6 +12,6 @@ end
   
   def take_a_number(deli, name)
     deli << name
-    puts "Welcome, #{name}. You are number 1 in line"
+    puts "Welcome, #{name}. You are #{deli.length} in line"
   end
   
