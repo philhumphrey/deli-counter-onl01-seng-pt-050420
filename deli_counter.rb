fea1075ae589ac02c58371c1c0e35f7c81778dc2
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-15 15:04:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ end
     if deli.empty?
       puts "There is nobody waiting to be served!"
     else
-      puts "Currently_serving #{name
+      puts "Currently_serving #{deli.front 
     else deli.shift
     end
   end
