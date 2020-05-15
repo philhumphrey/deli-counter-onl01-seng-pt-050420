94548cefbf82951acf41186018c53dbb52badad6
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-15 14:33:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(deli)
     puts "The line is currently empty"
   else 
     current_line = "The line is currently: "
-    deli.each_with_index do |customer|
+    deli.each_with_index do |customer, index|
       current_line << ""
   end
 end
