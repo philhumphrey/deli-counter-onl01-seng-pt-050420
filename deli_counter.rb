be078a04686d97acf0c59f7a86524cdba66e5404
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-15 14:26:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,8 @@ def line(deli)
   end
 end
   
-  def take_a_number(katz_deli, end_of_line)
-    katz_deli = []
+  # def take_a_number(katz_deli, end_of_line)
+  #   katz_deli = []
     
-    def now_serving
-      puts
+  #   def now_serving
+  #     puts
